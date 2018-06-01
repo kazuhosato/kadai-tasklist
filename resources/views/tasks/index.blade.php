546d6e3b1378ca0333414fd799ef9120d6a16db6
kazuho.sato <ec2-user@ip-172-31-14-23.us-east-2.compute.internal>
2018-06-01 04:01:39 +0000

    complete

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 
-    <h1>Do NOT FORGET YOUR DUTY</h1>
+    <h1>All Tasks</h1>
 
     @if (count($tasks) > 0)
         <ul>
             @foreach ($tasks as $task)
-              <li>{!! link_to_route('tasks.show', $task->created_at, ['id' => $task->id]) !!} > {{ $task->status }} > {{ $task->content }}</li> 
+              <li>{!! link_to_route('tasks.show', $task->updated_at, ['id' => $task->id]) !!} > {{ $task->status }} > {{ $task->content }}</li> 
                 
             @endforeach
         </ul>
