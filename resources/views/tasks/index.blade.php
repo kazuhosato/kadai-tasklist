d20d95c478c07251e6bb0ebbca5ad1c4bd9f6100
kazuho.sato <ec2-user@ip-172-31-14-23.us-east-2.compute.internal>
2018-06-04 04:08:49 +0000

    aa

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -25,6 +25,6 @@
         </table>
     @endif
 
-    {!! link_to_route('tasks.create', 'New Task', null, ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.create', '   New Task', null, ['class' => 'btn btn-primary glyphicon glyphicon-plus ']) !!}
 
 @endsection
